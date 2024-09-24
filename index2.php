12b290bf2ef24e4757633b62eaa0a19d5a029592
Gabriel Mariotti <gabrielmariotti44@gmail.com>
2024-09-24 11:59:18 +0100

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,1 +1,23 @@
-
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+        Nome: <input type="text" name="fnome">
+        <input type="submit">
+    </form>
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $nome = $_REQUEST['fnome'];
+            if (empty($nome)){
+                echo "Nome não prenchido";
+            }else{
+                echo "<p>Bom dia". $nome ."</p>";
+            }
+        }
+    ?>
+</body>
+</html>
