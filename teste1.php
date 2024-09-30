2a1a2cd6a332d1420b32a27eccd095f058c5dd51
Gabriel Mariotti <gabrielmariotti44@gmail.com>
2024-09-30 11:39:00 +0100

    Add files via upload

---
+++ teste1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Bem-vindo <?php echo $_POST["nome"];?><br>
+    o seu email é: <?php echo $_POST["email"];?><br>
+    O seu website é: <?php echo $_POST["website"];?><br>
+    o seu genero é: <?php echo $_POST["genero"];?><br>
+    O seu comentario é: <?php echo $_POST["coment"];?><br>
+</body>
+</html>
