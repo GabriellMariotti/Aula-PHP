a613bef070e05d911e069fed0b97c58b11fd9bba
Gabriel Mariotti <gabrielmariotti44@gmail.com>
2024-10-01 11:42:01 +0100

    Add files via upload
    01-10-2024

---
+++ cookies1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $cookie_nome = "user";
+        $cookie_valor = "Maria Fernanda";
+
+        setcookie($cookie_nome, $cookie_valor, time() + (86400 * 30), "/");
+    ?>
+    <?php
+        if(!isset($_COOKIE[$cookie_nome])){
+            echo "Cookie". $cookie_nome. " não atribuido!";
+        }else{
+            echo "Cookie ". $cookie_nome ." está atribuido!<br>";
+            echo "O valor é: ". $_COOKIE[$cookie_nome];
+        }
+
+    ?>
+</body>
+</html>
